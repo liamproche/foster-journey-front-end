38969d5ed5854758dc4753f18880f55e5883cdea
Liam Roche <roche.liamp@gmail.com>
2022-04-12 19:15:14 -0400

    started placement edit form

--- src/PlacementComponent/IndividualPlacementComponent/DetailsComponent/EditFormComponent/EditFormComponent.jsx
+++ src/PlacementComponent/IndividualPlacementComponent/DetailsComponent/EditFormComponent/EditFormComponent.jsx
@@ -6,7 +6,26 @@ import './EditFormComponent.css'
 function EditFormComponent(props) {
     return (
       <div className="EditFormComponent">
-          <h1>THIS IS THE EDIT FORM</h1>
+          <h1>Edit a placement:</h1>
+          <form id="placement-form">
+                <label htmlFor="num">Placement Number:</label>
+                <input type="number" name="num" min="1" required value={props.placement.num}></input>
+                <br/>
+                <label htmlFor="name">Create a name for this placement</label>
+                <input type="text" name="name" value={props.placement.name}></input>
+                <br/>
+                <label htmlFor="start_date">Start Date</label>
+                <input type="date" name="start_date" value={props.placement.start_date}></input>
+                <br/>
+                <label htmlFor="end_date">End Date</label>
+                <input type="date" name="end_date" value={props.placement.end_date}></input>
+                <br/>
+                <label htmlFor="location">Location</label>
+                <input type="text" name="location" value={props.placement.location}></input>
+                <br/>
+                <br/>
+                <button type="Submit">Submit Placement</button>
+            </form>
           <button onClick={()=>{
             props.deletePlacement(props.placement.id)
           }}>Delete placement</button>
