630b240be3133b2e005c15f3df986792e6b3eecd
Liam Roche <roche.liamp@gmail.com>
2022-04-15 19:46:56 -0400

    minor ug fix

--- src/NavBarComponent/Nav.jsx
+++ src/NavBarComponent/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
@@ -13,17 +13,17 @@ function NavBar(){
   return (
     <nav>
       <h1>Foster Journey Nav</h1>
-
+      <Link to="/">Home</Link>
       <span>  |  </span>
       {user ? (
-        <p onClick={logoutUser}>Logout</p>,
-        <Link to="/">Placements</Link>
+        <p onClick={logoutUser}>Logout</p>
       ):(
         <Link to="/login">Login</Link>
       )}
       <span>  |  </span>
       <Link to="/register">Create Account</Link>
       {user && <p>Hello, {user.user_id}</p>}
+
     </nav>
   );
 };
